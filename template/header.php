7418498dd86375eab058cd85a79df0438f9462cb
Giw <giw2000y@gmail.com>
2023-12-05 21:19:00 +0700

    header.php add shadow in navbar

--- template/header.php
+++ template/header.php
@@ -1,4 +1,4 @@
-<div class="container-fluid d-flex flex-column px-0">
+<div class="container-fluid d-flex flex-column px-0 shadow-sm">
         <div class="d-flex justify-content-between align-items-center px-5 py-3">
             <div class="d-flex align-items-center">
                 <a href="/FinalProj/" class="navbar-brand"><img class="" src="img/rmuttlogo.png" alt="rmutt" height="65"></a>
