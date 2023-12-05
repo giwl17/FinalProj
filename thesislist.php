0223ca53a9cb3a90ef8153f5aed544b1c7383062
Giw <giw2000y@gmail.com>
2023-12-05 20:37:36 +0700

    move code in index.php to new file thesislist.php

---
+++ thesislist.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RMUTT</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <?php require "template/header.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
