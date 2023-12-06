b00bf76cbed8c50602c599854c90d4b37fc1ec9d
Giw <giw2000y@gmail.com>
2023-12-06 23:21:20 +0700

    ui thesis_add.php finisted

--- thesis_add.php
+++ thesis_add.php
@@ -28,17 +28,205 @@
 
         <div>
             <p>คณะผู้จัดทำ</p>
-            
+
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" name="" id="">
                 <label class="form-check-label" for="">สมาชิกคนที่ 1</label>
+                <div>
+                    <div class="form-group mb-3">
+                        <label for="">รหัสนักศึกษา</label>
+                        <input class="form-control" type="text" name="" id="">
+                    </div>
+                    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3 mb-3">
+                        <div class="col form-group">
+                            <label for="">คำนำหน้า</label>
+                            <select class="form-select" name="" id="">
+                                <option value=""></option>
+                                <option value="นาย">นาย</option>
+                                <option value="นางสาว">นางสาว</option>
+                                <option value="นาง">นาง</option>
+                            </select>
+                        </div>
+                        <div class="col form-group">
+                            <label for="">ชื่อ</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                        <div class="col form-group">
+                            <label for="">นามสกุล</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                    </div>
+                </div>
             </div>
 
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="" id="">
+                <label class="form-check-label" for="">สมาชิกคนที่ 2</label>
+                <div>
+                    <div class="form-group mb-3">
+                        <label for="">รหัสนักศึกษา</label>
+                        <input class="form-control" type="text" name="" id="">
+                    </div>
+                    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3 mb-3">
+                        <div class="col form-group">
+                            <label for="">คำนำหน้า</label>
+                            <select class="form-select" name="" id="">
+                                <option value=""></option>
+                                <option value="นาย">นาย</option>
+                                <option value="นางสาว">นางสาว</option>
+                                <option value="นาง">นาง</option>
+                            </select>
+                        </div>
+                        <div class="col form-group">
+                            <label for="">ชื่อ</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                        <div class="col form-group">
+                            <label for="">นามสกุล</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="" id="">
+                <label class="form-check-label" for="">สมาชิกคนที่ 3</label>
+                <div>
+                    <div class="form-group mb-3">
+                        <label for="">รหัสนักศึกษา</label>
+                        <input class="form-control" type="text" name="" id="">
+                    </div>
+                    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3 mb-3">
+                        <div class="col form-group">
+                            <label for="">คำนำหน้า</label>
+                            <select class="form-select" name="" id="">
+                                <option value=""></option>
+                                <option value="นาย">นาย</option>
+                                <option value="นางสาว">นางสาว</option>
+                                <option value="นาง">นาง</option>
+                            </select>
+                        </div>
+                        <div class="col form-group">
+                            <label for="">ชื่อ</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                        <div class="col form-group">
+                            <label for="">นามสกุล</label>
+                            <input class="form-control" type="text" name="" id="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="">อาจารย์ที่ปรึกษาหลัก</label>
+            <select class="form-select" name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+
+        <div class="form-group mb-3">
+            <label class="" for="">อาจารย์ที่ปรึกษาร่วม</label>
+            <select class="form-select" name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+
+        <div class="form-group mb-3">
+            <label class="" for="">ประธานกรรมการ</label>
+            <select class="form-select" name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+        <div class="form-group mb-3">
+            <label class="" for="">กรรมการคนที่ 1</label>
+            <select class="form-select" name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+        <div class="form-group mb-3">
+            <label class="" for="">กรรมการคนที่ 2</label>
+            <select class="form-select" name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+
+        <div class="row align-items-center mb-3">
+            <div class="col-auto">
+                <div class="col-form-label">ปีการศึกษาที่ทำเสร็จ</div>
+            </div>
+            <div class="col-auto">
+                <select name="" id="" class="form-select">
+                    <option value=""></option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
+            <div class="col-auto">
+                <div class="col-form-label">/</div>
+            </div>
+            <div class="col-auto">
+                <input class="form-control" type="number" name="" id="" maxlength="4">
+            </div>
+
+        </div>
+
+        <div class="form-group" id="formWord">
+            <label for="">คำสำคัญ (อย่างน้อย 3 คำ)</label>
+            <input class="form-control mb-3" type="text" name="" id="word_1" placeholder="คำสำคัญ 1">
+            <input class="form-control mb-3" type="text" name="" id="word_2" placeholder="คำสำคัญ 2">
+            <input class="form-control mb-3" type="text" name="" id="word_3" placeholder="คำสำคัญ 3">
+        </div>
+        <div class="mb-3">
+            <input class="btn btn-success" type="button" value="+" id="buttonAddWord">
+            <input class="btn btn-danger" type="button" value="-" id="buttonDeleteWord">
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label">ไฟล์หน้าอนุมัติ (ชนิดไฟล์ PDF)</label>
+            <input class="form-control" type="file" id="">
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label">ไฟล์เล่มปริญญานิพนธ์ (ชนิดไฟล์ PDF)</label>
+            <input class="form-control" type="file" id="">
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label">ไฟล์โปสเตอร์ (ชนิดไฟล์ PDF)</label>
+            <input class="form-control" type="file" id="">
         </div>
         <input class="btn btn-primary container-fluid" type="submit" value="เพิ่มข้อมูล">
     </form>
 
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        let buttonAddWord = document.getElementById('buttonAddWord');
+        let buttonDeleteWord = document.getElementById('buttonDeleteWord');
+        let formWord = document.getElementById('formWord');
+        let i = 4;
+        buttonAddWord.addEventListener('click', () => {
+            $html = ' <input class="form-control mb-3" type="text" name="" id="word_' + i + '" placeholder="คำสำคัญ ' + i + '">';
+            formWord.insertAdjacentHTML('beforeend', $html);
+            i++;
+            console.log('i=', i);
+        })
+        buttonDeleteWord.addEventListener('click', () => {
+            console.log('click');
+            if (i > 4) {
+                let id = 'word_' + (i - 1);
+                console.log(id);
+                console.log(document.querySelector(id));
+                let currentDelete = document.getElementById(id);
+                currentDelete.remove();
+                i--;
+            }
+        })
+    </script>
 </body>
 
 </html>
