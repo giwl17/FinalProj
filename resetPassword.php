abe30886fb7db83cc658047318055e95d51e1ec9
Tanwa017 <khathawutoon1@gmail.com>
2023-12-07 16:30:16 +0700

    add file

--- resetPassword.php
+++ resetPassword.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login page</title>
+    <title>Reset Password Page</title>
 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
@@ -13,20 +13,14 @@
 <body>
     <?php require 'template/header_login.php'; ?>
         <form class="container mt-4" action="" method="POST">
-            <h1 class="h1 text-center">เข้าสู่ระบบ</h1>
+            <h1 class="h1 text-center">ลืมรหัสผ่าน</h1>
             <div class="form-group mb-3">
                 <label for="email">Email</label>
                 <input class="form-control" type="email" name="email" placeholder="Enter email" required>
             </div>
-            <div class="form-group mb-3">
-                <label for="pass">Password</label>
-                <input class="form-control" type="password" name="pass" placeholder="Password" required>
-                <div class="form-text">
-                หากลืมรหัสผ่าน <span><a href="/FinalProj/resetPassword.php">คลิกที่นี่</a></span>
-            </div>
-            </div>
             
-            <input type="submit" class="btn btn-primary container-fluid mb-3" value="เข้าสู่ระบบ" />
+            
+            <input type="submit" class="btn btn-primary container-fluid" value="เข้าสู่ระบบ" />
         </form>
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
