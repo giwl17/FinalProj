b2665758010b51b5ab696fbe0cba4ac83ef2464a
Tanwa017 <khathawutoon1@gmail.com>
2023-12-12 01:31:29 +0700

    add confirm_password

--- hashPass_db.php
+++ hashPass_db.php
@@ -5,18 +5,21 @@ session_start();
 date_default_timezone_set('asia/bangkok');
 
 if (isset($_POST["confirm"])) {
+    // $pass = $_POST["pass"];
+    $confirm = $_POST["confirm"];
+    $hashPass = password_hash($confirm, PASSWORD_DEFAULT);
 
-    $pass = $_POST["confirm"];
-    $hashPass = password_hash($pass, PASSWORD_DEFAULT);
     try {
         $update =  $conn->prepare("UPDATE account SET password = '$hashPass' WHERE email = 'tanwa@gmail.com'");
-        // $update = "UPDATE account SET password = '$pass' WHERE email = 'tanwa@gmail.com'";
+        // $update = $conn->prepare("UPDATE account SET password = '$confirm' WHERE email = 'tanwa@gmail.com'");
 
         $result = $update->execute();
         // $result = mysqli_query($conn, $update);
     } catch (PDOException $e) {
         echo $e;
     }
+
+
     $conn = null;
 }
 
