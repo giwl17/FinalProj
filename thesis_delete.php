bbef5b5b9b8b6bcb37577e1ab4ff6b6f2dc76e74
Giw <giw2000y@gmail.com>
2023-12-14 01:12:34 +0700

    add loaction back to thesislist

--- thesis_delete.php
+++ thesis_delete.php
@@ -7,6 +7,7 @@
             $stmt = $conn->prepare("UPDATE thesis_document SET thesis_status = 0 WHERE thesis_id = :thesis_id");
             $stmt->bindParam(":thesis_id", $id);
             $stmt->execute();
+            header('location: /FinalProj');
         } catch(PDOException $e) {
             echo $e->getMessage();
         }
