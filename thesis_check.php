fe27c16e67eb3bd90b8dddec9391c1ea2a845a6d
Giw <giw2000y@gmail.com>
2024-01-16 09:30:21 +0700

    add file thesis_check.php and add route

---
+++ thesis_check.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>จัดการเล่มปริญญานิพนธ์ : ตรวจสอบข้อมูล</title>
+    <link rel="icon" type="image/x-icon" href="./img/rmuttlogo16x16.jpg">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <?php require "template/header.php" ?>
+    <div class="container my-5 d-flex justify-content-center">
+        <h1 class="h3 text-primary">รายการที่รอตรวจสอบข้อมูล</h1>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
