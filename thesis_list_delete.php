77fc8b3627dd2019d93594d294c8fc10916eb9ee
Giw <giw2000y@gmail.com>
2024-02-08 14:07:57 +0700

    search

--- thesis_list_delete.php
+++ thesis_list_delete.php
@@ -11,8 +11,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="sweetalert2.min.css">
 </head>
 
 <body>
@@ -32,19 +30,25 @@
             $page = 1;
         }
         $start_from = ($page - 1) * $per_page_record;
-        $stmt = $conn->prepare("SELECT * FROM thesis_document WHERE thesis_status = 1 LIMIT {$start_from}, {$per_page_record}");
+        $stmt = $conn->prepare("SELECT * FROM thesis_document WHERE thesis_status = 1 ORDER BY thesis_id DESC LIMIT {$start_from}, {$per_page_record} ");
         $result = $stmt->execute();
         ?>
         <form method="POST" id="formSelectPage" action="/FinalProj/thesisdelete">
-            <div class="d-flex flex-column justify-content-end align-items-end">
-                <label class="w-25" for="selectShow">แสดงรายการ</label>
-                <select class="form-select w-25" id="selectShow" name="selectShow" onchange="showPages(this)">
-                    <option value="5" <?php if ($per_page_record == 5) echo "selected" ?>>5</option>
-                    <option value="10" <?php if ($per_page_record == 10) echo "selected" ?>>10</option>
-                    <option value="30" <?php if ($per_page_record == 30) echo "selected" ?>>30</option>
-                    <option value="50" <?php if ($per_page_record == 50) echo "selected" ?>>50</option>
-                    <option value="100" <?php if ($per_page_record == 100) echo "selected" ?>>100</option>
-                </select>
+            <div class="d-flex justify-content-between align-items-center">
+                <div>
+                    <label class="" for="search">ค้นหาชื่อปริญญานิพนธ์</label>
+                    <input class="form-control" type="search" name="search" id="search" onchange="searchThesis()">
+                </div>
+                <div>
+                    <label class="" for="selectShow">แสดงรายการ</label>
+                    <select class="form-select" id="selectShow" name="selectShow" onchange="showPages(this)">
+                        <option value="5" <?php if ($per_page_record == 5) echo "selected" ?>>5</option>
+                        <option value="10" <?php if ($per_page_record == 10) echo "selected" ?>>10</option>
+                        <option value="30" <?php if ($per_page_record == 30) echo "selected" ?>>30</option>
+                        <option value="50" <?php if ($per_page_record == 50) echo "selected" ?>>50</option>
+                        <option value="100" <?php if ($per_page_record == 100) echo "selected" ?>>100</option>
+                    </select>
+                </div>
             </div>
         </form>
         <table class="table table-hover">
@@ -57,7 +61,7 @@
                     <th>ปีการศึกษา</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="tbody">
                 <?php foreach ($stmt as $row) : ?>
                     <tr>
                         <td><input type="checkbox" name="select_<?= $row['thesis_id'] ?>" class="select"></td>
@@ -71,14 +75,13 @@
         </table>
 
         <nav class="d-flex justify-content-center">
-            <ul class="pagination">
+            <ul class="pagination" id="pagination">
                 <?php
                 $rs_result = $conn->prepare("SELECT * FROM thesis_document WHERE thesis_status = 1");
                 $rs_result->execute();
                 $row = $rs_result->fetchAll();
                 $total_records = $rs_result->rowCount();
 
-                echo "</br>";
                 // Number of pages required.   
                 $total_pages = ceil($total_records / $per_page_record);
                 $pagLink = "";
@@ -147,8 +150,6 @@
             } else {
                 let checkedList = [];
                 selectItems.forEach(item => {
-                    console.log(item.name);
-                    console.log(item.checked);
                     if (item.checked)
                         checkedList.push(item.name);
                 })
@@ -184,9 +185,134 @@
         }
 
         function showPages(event) {
-            const form = document.getElementById("formSelectPage");
-            form.submit();
+            selectShow = document.querySelector('#selectShow').value;
+            let url = "/FinalProj/thesis_api?search=" + search.value + "&limit=" + selectShow;
+            fetch(url, {
+                    method: "GET"
+                })
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    let html = "";
+                    data.forEach(item => {
+                        html += `<td><input type='checkbox' name='select_${item.thesis_id}' class='select'></td>`
+                        html += `<td>${item.thai_name}</td>`
+                        html += `<td>${item.english_name}</td>`
+                        html += `<td>${item.printed_year}</td>`
+                        html += `<td>${item.semester}/${item.approval_year}</td>`
+                        html += "</tr>"
+                    })
+                    tbody.innerHTML = html;
+                    let pagination = document.querySelector('#pagination')
+                    let total_records;
+                    htmlPagebar = "";
+                    page = 1;
+                    url = `/FinalProj/thesis_api?search=${search.value}`
+                    fetch(url, {
+                            method: "GET"
+                        }).then(res => {
+                            return res.json()
+                        })
+                        .then(data => {
+                            total_records = data.length;
+                            let total_pages = Math.ceil(total_records / selectShow)
+                            console.log("selectShow ", selectShow)
+                            console.log("total_records ", total_records)
+                            console.log("total_pages ", total_pages);
+                            if (page >= 2) {
+                                htmlPagebar += `<li class='page-item'><a class='page-link' href='thesisdelete?page=${page - 1}'> < </a></li>`;
+                            }
+                            for (let i = 1; i <= total_pages; i++) {
+                                if (i == page) {
+                                    htmlPagebar += `<li class='page-item active'>
+                                <a class='page-link' href='thesisdelete?page=${i}'> ${i}</a>
+                                </li>`
+                                } else {
+                                    htmlPagebar += `<li class='page-item'>
+                                <a class='page-link' href='thesisdelete?page=${i}'> ${i}</a>
+                                </li>`
+                                }
+                            }
+                            if (page < total_pages) {
+                                htmlPagebar += `<li class='page-item'><a class='page-link' href='thesisdelete?page=${page + 1}'> > </a></li>`;
+                            }
+
+                            pagination.innerHTML = htmlPagebar;
+                        })
+                })
         }
+
+        //search
+        let search = document.querySelector("#search");
+        let tbody = document.querySelector('#tbody');
+        let selectShow = document.querySelector("#selectShow").value;
+        let page = 1;
+        const searchParams = new URLSearchParams(window.location.search);
+        if (searchParams.has('page')) {
+            page = searchParams.get('page')
+        } else {
+            page = 1;
+        }
+        search.addEventListener('keyup', () => {
+            page = 1;
+            let url = "/FinalProj/thesis_api?search=" + search.value + "&limit=" + selectShow;
+            fetch(url, {
+                    method: "GET"
+                })
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    let html = "";
+                    data.forEach(item => {
+                        html += `<td><input type='checkbox' name='select_${item.thesis_id}' class='select'></td>`
+                        html += `<td>${item.thai_name}</td>`
+                        html += `<td>${item.english_name}</td>`
+                        html += `<td>${item.printed_year}</td>`
+                        html += `<td>${item.semester}/${item.approval_year}</td>`
+                        html += "</tr>"
+                    })
+                    tbody.innerHTML = html;
+                    let pagination = document.querySelector('#pagination')
+                    let total_records;
+                    htmlPagebar = "";
+                    page = 1;
+                    url = `/FinalProj/thesis_api?search=${search.value}`
+                    fetch(url, {
+                            method: "GET"
+                        }).then(res => {
+                            return res.json()
+                        })
+                        .then(data => {
+                            total_records = data.length;
+                            let total_pages = Math.ceil(total_records / selectShow)
+                            console.log("selectShow ", selectShow)
+                            console.log("total_records ", total_records)
+                            console.log("total_pages ", total_pages);
+                            if (page >= 2) {
+                                htmlPagebar += `<li class='page-item'><a class='page-link' href='thesisdelete?page=${page - 1}'> < </a></li>`;
+                            }
+                            for (let i = 1; i <= total_pages; i++) {
+                                if (i == page) {
+                                    htmlPagebar += `<li class='page-item active'>
+                                <a class='page-link' href='thesisdelete?page=${i}'> ${i}</a>
+                                </li>`
+                                } else {
+                                    htmlPagebar += `<li class='page-item'>
+                                <a class='page-link' href='thesisdelete?page=${i}'> ${i}</a>
+                                </li>`
+                                }
+                            }
+                            if (page < total_pages) {
+                                htmlPagebar += `<li class='page-item'><a class='page-link' href='thesisdelete?page=${page + 1}'> > </a></li>`;
+                            }
+
+                            pagination.innerHTML = htmlPagebar;
+                        })
+                })
+
+        })
     </script>
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
