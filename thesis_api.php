76c071daa216ac56bc532908bbf4b86e572be505
Giw <giw2000y@gmail.com>
2024-02-08 14:18:06 +0700

    api search thesis name

--- thesis_api.php
+++ thesis_api.php
@@ -24,6 +24,30 @@ if (isset($_GET['id'])) {
     $thesis = json_encode($thesis, JSON_UNESCAPED_UNICODE);
 
     echo $thesis;
+} else if (isset($_GET['search']) && isset($_GET['limit'])) {
+    $search = $_GET['search'];
+    $like = "%" . $search . "%";
+    $limit = $_GET['limit'];
+
+    $sql = $conn->prepare("SELECT * FROM thesis_document
+    WHERE thai_name LIKE :like
+    OR english_nmae LIKE :like
+    LIMIT :limit
+    ORDER BY DESC");
+    $sql->bindParam(":like", $like);
+    $sql->bindParam(":limit", $limit);
+    $sql->execute();
+    $result = $sql->fetchAll(PDO::FETCH_OBJ);
+    $thesis = [];
+    foreach ($result as $row) {
+        $thesis['thai_name'] = $row->thai_name;
+        $thesis['english_name'] = $row->english_name;
+        $thesis['printed_year'] = $row->printed_year;
+        $thesis['semester'] = $row->semester;
+        $thesis['approval_year'] = $row->approval_year;
+    }
+    $thesis = json_encode($thesis, JSON_UNESCAPED_UNICODE);
+    echo $thesis;
 } else {
     $sql = $conn->prepare("SELECT * FROM thesis_document ORDER BY thesis_id ASC");
     $sql->execute();
@@ -39,7 +63,7 @@ if (isset($_GET['id'])) {
         $result_mem = $select_mem->fetchAll(PDO::FETCH_OBJ);
         $thesis[$index]['thai_name'] = $row->thai_name;
         $thesis[$index]['english_name'] = $row->english_name;
-        foreach($result_mem as $mem) {
+        foreach ($result_mem as $mem) {
             $thesis[$index]['author_member']["member$i"]["student_id"] = $mem->student_id;
             $thesis[$index]['author_member']["member$i"]["name"] = $mem->name;
             $thesis[$index]['author_member']["member$i"]["lastname"] = $mem->lastname;
