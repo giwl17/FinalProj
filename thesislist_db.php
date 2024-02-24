4a76357ae6c26ef6b2daac219c08700c7ceaeb91
Giw <giw2000y@gmail.com>
2024-02-25 00:48:40 +0700

    change ui thesis list

--- thesislist_db.php
+++ thesislist_db.php
@@ -1,60 +1,63 @@
 <?php
-    require_once "dbconnect.php";
-    $select = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY thesis_id DESC";
-    $stmt = $conn->prepare($select);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_OBJ);
-    if ($stmt->rowCount() > 0) {
-        foreach ($result as $row) {
-            $html = "
-                    <div class='border border-dark w-100 p-3 d-flex flex-column'>
+require_once "dbconnect.php";
+$select = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY thesis_id DESC";
+$stmt = $conn->prepare($select);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_OBJ);
+$html =
+<<<HTML
+    <div class='d-flex gap-3 flex-wrap'> 
+ HTML;
+if ($stmt->rowCount() > 0) {
+    foreach ($result as $row) {
+        $html .=
+            <<<HTML
+                    <div class='w-100 border border-secondary-subtle p-3 d-flex flex-column rounded-3'>
                         <a class='text-dark' id='thesisName' href='thesis?id=$row->thesis_id'>
                         <div class='fw-bold'>$row->thai_name</div>
                         <div class='fw-bold'>$row->english_name</div>
                         </a>
                         <div>คณะผู้จัดทำ           
-            ";
+            HTML;
 
-            $selectMem = $conn->prepare("SELECT * FROM author_thesis WHERE thesis_id = $row->thesis_id");
-            $selectMem->execute();
-            $result_selectMem = $selectMem->fetchAll(PDO::FETCH_OBJ);
+        $selectMem = $conn->prepare("SELECT * FROM author_thesis WHERE thesis_id = $row->thesis_id");
+        $selectMem->execute();
+        $result_selectMem = $selectMem->fetchAll(PDO::FETCH_OBJ);
 
-            if ($selectMem->rowCount() > 0) {
-                $count = count($result_selectMem);
-                $i = 1;
-                foreach ($result_selectMem as $mem) {
-                    $nameAuthor = $mem->prefix . "" . $mem->name . " " . $mem->lastname;
-                    $html .= "<div class='d-inline'>$nameAuthor</div>";
-                    if ($count != $i++) {
-                        $html .= "<span class='text-dark'>, </span>";
-                    }
+        if ($selectMem->rowCount() > 0) {
+            $count = count($result_selectMem);
+            $i = 1;
+            foreach ($result_selectMem as $mem) {
+                $nameAuthor = $mem->prefix . "" . $mem->name . " " . $mem->lastname;
+                $html .= "<div class='d-inline'>$nameAuthor</div>";
+                if ($count != $i++) {
+                    $html .= "<span class='text-dark'>, </span>";
                 }
             }
-            $u = '_';
-            $html .= "</div>";  
-            $html .= "<div>อาจารยที่ปรึกษา <a href='search?advisor=$row->prefix_advisor$u$row->name_advisor$u$row->surname_advisor' class='link-primary' style='text-decoration:none;'>$row->prefix_advisor $row->name_advisor $row->surname_advisor</a>";
-            if($row->prefix_coAdvisor != '') {
-                $html .= ", ";
-                $html .= "<a href='search?coAdvisor=$row->prefix_coAdvisor$u$row->name_coAdvisor$u$row->surname_coAdvisor' class='link-primary' style='text-decoration:none;'>$row->prefix_coAdvisor $row->name_coAdvisor $row->surname_coAdvisor</a>";
-            }
-            $html .= "</div>";
-
-            // $html .= "<div>คำสำคัญ <a href='#' class='link-primary' style='text-decoration:none;'>$row->keyword</a></div>";            
-            $keyword = explode(", ", $row->keyword);
-            $html .= "<div class='col-auto d-flex flex-row'>คำสำคัญ&nbsp";
-            for ($i = 0; $i < count($keyword); $i++) {
-                $html .= "<a style='text-decoration:none;' href='search?keyword=$keyword[$i]'>$keyword[$i]</a>";
-                if (!($i == count($keyword) - 1)) {
-                    $html .= ",&nbsp";
-                }
-            }   
-            $html .= "</div>";
-
-            $html .= "<div>ปีที่พิมพ์เล่ม <a href='search?printed=$row->printed_year' class='link-primary' style='text-decoration:none;'>$row->printed_year</a></div>";
-            $html .= "</div>";
-
-            echo $html;
         }
-      
+        $u = '_';
+        $html .= "</div>";
+        $html .= "<div>อาจารยที่ปรึกษา <a href='search?advisor=$row->prefix_advisor$u$row->name_advisor$u$row->surname_advisor' class='link-primary' style='text-decoration:none;'>$row->prefix_advisor $row->name_advisor $row->surname_advisor</a>";
+        if ($row->prefix_coAdvisor != '') {
+            $html .= ", ";
+            $html .= "<a href='search?coAdvisor=$row->prefix_coAdvisor$u$row->name_coAdvisor$u$row->surname_coAdvisor' class='link-primary' style='text-decoration:none;'>$row->prefix_coAdvisor $row->name_coAdvisor $row->surname_coAdvisor</a>";
+        }
+        $html .= "</div>";
+
+        // $html .= "<div>คำสำคัญ <a href='#' class='link-primary' style='text-decoration:none;'>$row->keyword</a></div>";            
+        $keyword = explode(", ", $row->keyword);
+        $html .= "<div class='col-auto d-flex flex-row'>คำสำคัญ&nbsp";
+        for ($i = 0; $i < count($keyword); $i++) {
+            $html .= "<a style='text-decoration:none;' href='search?keyword=$keyword[$i]'>$keyword[$i]</a>";
+            if (!($i == count($keyword) - 1)) {
+                $html .= ",&nbsp";
+            }
+        }
+        $html .= "</div>";
+
+        $html .= "<div>ปีที่พิมพ์เล่ม <a href='search?printed=$row->printed_year' class='link-primary' style='text-decoration:none;'>$row->printed_year</a></div>";
+        $html .= "</div>";
     }
-?>
+}
+$html .= "</div>";
+echo $html;
