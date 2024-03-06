f3521369943395171b15adc82c8e0c17a34cbc43
Giw <giw2000y@gmail.com>
2024-03-06 11:50:51 +0700

    change show number thesismanage permiss in teach role

--- manage_privilege.php
+++ manage_privilege.php
@@ -1,6 +1,6 @@
 <?php
-include 'dbconnect.php';
 session_start();
+include 'dbconnect.php';
 $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
 // if (isset($_SESSION['role'])) {
 //     $name = $_SESSION['name'] . "&nbsp" . $_SESSION['lastname'];
@@ -110,7 +110,7 @@ if ($_SESSION['role'] == 1) {
                                         <td><?= $row['prefix'] . $row['name'] . "&nbsp" . $row['lastname'] ?></td>
                                         <td><?= $row['email'] ?></td>
                                         <td><input type="checkbox" name="members_<?= $row['account_id'] ?>" value='1' <?= ($row['member_manage_permission'] == 1 ? 'checked' : ''); ?> class="membersTeacher"><?= $row['member_manage_permission'] ?></td>
-                                        <td><input type="checkbox" name="document_<?= $row['account_id'] ?>" value='1' <?= ($row['thesis_manage_permission'] == 1 ? 'checked' : ''); ?> class="documentTeacher"><?= $row['thesis_manage_permission'] ?></td>
+                                        <td><input type="checkbox" name="document_<?= $row['account_id'] ?>" value='1' <?= ($row['thesis_manage_permission'] == 1 ? 'checked' : ''); ?> class="documentTeacher"><?= $row['thesis_manage_permission'] != NULL ? $row['thesis_manage_permission'] : '0' ?></td>
                                         <td><input type="checkbox" name="teacher_<?= $row['account_id'] ?>" value='1' <?= ($row['status'] == 1 ? 'checked' : ''); ?> class="statusTeacher"><?= $row['status'] ?></td>
                                     </tr>
                                 <?php endforeach; ?>
@@ -192,8 +192,8 @@ if ($_SESSION['role'] == 1) {
         </div>
     <?php
 } else {
-    header("Location: /FinalProj/");
-    exit();
+    // header("Location: /FinalProj/");
+    // exit();
 }
 // }
     ?>
