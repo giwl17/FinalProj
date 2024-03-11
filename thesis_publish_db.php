747318c2365bf6312bd53584432c3fe1e8411e96
Giw <giw2000y@gmail.com>
2024-03-11 19:57:55 +0700

    reset dateTime_Deleted in database

--- thesis_publish_db.php
+++ thesis_publish_db.php
@@ -3,7 +3,7 @@ require "dbconnect.php";
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     try {
-        $stmt = $conn->prepare("UPDATE thesis_document SET thesis_status = 1 WHERE thesis_id = :thesis_id");
+        $stmt = $conn->prepare("UPDATE thesis_document SET thesis_status = 1, dateTime_deleted = '' WHERE thesis_id = :thesis_id");
         $stmt->bindParam(":thesis_id", $id);
         $stmt->execute();
         header('location: /FinalProj');
@@ -18,7 +18,7 @@ if (isset($_GET['id'])) {
         $data_ex = array_pop($data_ex);
         array_push($id, $data_ex);
     }
-    $sql = "UPDATE thesis_document SET thesis_status = 1 WHERE thesis_id = $id[0]";
+    $sql = "UPDATE thesis_document SET thesis_status = 1, dateTime_deleted = '' WHERE thesis_id = $id[0]";
     for ($i = 1; $i < count($id); $i++) {
         $sql .= " OR thesis_id = $id[$i]";
     }
