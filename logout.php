32966aec94c6760afb99762b4461d276294fa9fa
Tanwa017 <khathawutoon1@gmail.com>
2024-03-14 19:35:23 +0700

    Edit logout v.1

--- logout.php
+++ logout.php
@@ -5,33 +5,3 @@ session_destroy();
 setcookie('email', '', time() - 3600, '/'); // ทำลาย Cookie
 header("Location: /FinalProj/"); // Redirect ไปที่หน้า login
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Logout</title>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-</head>
-<body>
-    <script>
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'You want to logout?',
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, logout!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire(
-                    'Logged Out!',
-                    'You have been logged out.',
-                    'success'
-                ).then(function() {
-                    window.location = 'login.html'; // หรือไปยังหน้าที่ต้องการหลังจาก logout
-                });
-            }
-        });
-    </script>
-</body>
-</html> -->
