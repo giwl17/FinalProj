bda6ef8019b6756638624c820e7b893649aa8d7f
Giw <giw2000y@gmail.com>
2024-03-17 00:34:53 +0700

    query thesis by sort and change menu method to post

--- thesislist_db.php
+++ thesislist_db.php
@@ -3,36 +3,50 @@
 require "dbconnect.php";
 
 //check page select
-if (isset($_GET["page"])) {
-    $page  = $_GET["page"];
+if (isset($_POST["page"])) {
+    $page  = $_POST["page"];
 } else {
     $page = 1;
 }
 $per_page_record = 10;
 $start_from = ($page - 1) * $per_page_record;
 
-//query thesis
-$query = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY thesis_id DESC LIMIT $start_from, $per_page_record";
-$stmt = $conn->prepare($query);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+if ($_POST['selectSortBy'] == 'sort_printedYear_new') {
+    //query thesis
+    $query = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY printed_year DESC LIMIT $start_from, $per_page_record";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else if ($_POST['selectSortBy'] == 'sort_printedYear_old') {
+    //query thesis
+    $query = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY printed_year ASC LIMIT $start_from, $per_page_record";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    //query thesis
+    $query = "SELECT * FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1 ORDER BY printed_year DESC LIMIT $start_from, $per_page_record";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 //count total record
 $query = "SELECT COUNT(*) as count FROM thesis_document WHERE thesis_status = 1 AND approval_status = 1";
 $selectAll = $conn->prepare($query);
 $selectAll->execute();
 $row_count =  $selectAll->fetch();
 $all_result =  $row_count['count'];
-
 ?>
 
-<div class='row align-items-center justify-content-end gap-1'>
-    <label class='w-auto' for='sortBy'>เรียงโดย</label>
-    <select class='form-select w-auto' id='sortBy' name='sortBy'>
-        <option>ปีที่ตีพิมพ์เล่ม ใหม่->เก่า</option>
-        <option>ปีที่ตีพิมพ์เล่ม เก่า->ใหญ่</option>
-    </select>
-</div>
+<form method="POST" action="" id="formSort">
+    <div class='row align-items-center justify-content-end gap-1'>
+        <label class='w-auto' for='sortBy'>เรียงโดย</label>
+        <select class='form-select w-auto' id='selectSortBy' name='selectSortBy' onchange="document.getElementById('formSort').submit();">
+            <option value="sort_printedYear_new" <?php if ($_POST['selectSortBy'] == 'sort_printedYear_new') echo "selected"; ?>>ปีที่ตีพิมพ์เล่ม ใหม่->เก่า</option>
+            <option value="sort_printedYear_old" <?php if ($_POST['selectSortBy'] == 'sort_printedYear_old') echo "selected"; ?>>ปีที่ตีพิมพ์เล่ม เก่า->ใหม่</option>
+        </select>
+    </div>
+</form>
 
 <!-- display list of thesis -->
 <div class='d-flex flex-column gap-3' id='thesis_list'>
@@ -98,29 +112,45 @@ $all_result =  $row_count['count'];
 $total_pages = ceil($all_result / $per_page_record);
 $pagLink = "";
 ?>
+
+
 <nav class='d-flex justify-content-center'>
     <ul class='pagination d-flex flex-wrap' id='pagination'>
 
         <?php if ($page >= 2) : ?>
-            <li class='page-item'><a class='page-link' href="?page=<?= ($page - 1) ?>">
-                    < </a>
-            </li>
+            <form action="" method="POST">
+                <li class='page-item'>
+                    <input type="hidden" name="page" value="<?= ($page - 1) ?>">
+                    <button class='page-link rounded-0 rounded-start-3 '> < </button>
+                </li>
+            </form>
         <?php endif; ?>
 
         <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
             <?php if ($i == $page) : ?>
+                <form action="" method="POST">
                 <li class='page-item active mb-1'>
-                    <a class='page-link' href='?page=<?= $i ?>'><?= $i ?></a>
+                    <input type="hidden" name="page" value="<?= $i ?>">
+                    <button class='page-link rounded-0'> <?= $i ?> </button>
                 </li>
+                </form>
             <?php else : ?>
+                <form action="" method="POST">
                 <li class='page-item'>
-                    <a class='page-link' href='?page=<?= $i ?>'><?= $i ?></a>
+                    <input type="hidden" name="page" value="<?= $i ?>">
+                    <button class='page-link rounded-0'> <?= $i ?> </button>
                 </li>
+                </form>
             <?php endif; ?>
         <?php endfor; ?>
 
         <?php if ($page < $total_pages) : ?>
-            <li class='page-item'><a class='page-link' href='?page=<?= ($page + 1) ?>'> > </a></li>
+            <form action="" method="POST">
+            <li class='page-item'>
+                <input type="hidden" name="page" value="<?= ($page + 1) ?>">
+                <button class='page-link rounded-0 rounded-end-3'> > </button>
+            </li>
+            </form>
         <?php endif; ?>
 
     </ul>
